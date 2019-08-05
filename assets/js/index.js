0785582aa81d43deb44e46ddc009e78156966cfe
yada <yarmdan@gmail.com>
2019-08-05 15:58:47 +0300

    feat: enable react on main page

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+// lib
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <span>Hello</span>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('react-root'));
